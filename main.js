95911e45823afbef5aca93ca633589ef5ebfbc81
José Roascio <jroascio95@gmail.com>
2022-09-21 23:12:31 -0300

    segunda entrega corregida

--- main.js
+++ main.js
@@ -8,15 +8,14 @@ class Usuario {
 }
 
 class Prestamo {
-    constructor(monto,cuotas) {
-        this.monto = monto;
-        this.cuotas = cuotas;
-    }
+  constructor(monto, cuotas) {
+    this.monto = monto;
+    this.cuotas = cuotas;
+  }
 }
 
 // Aca siempre van a estar mis prestamos
-const prestamos = [];
-
+let prestamos = [];
 
 const titulo = document.getElementById("titulo");
 titulo.innerHTML = "JOMARO - CASA DE PRESTAMOS";
@@ -37,19 +36,19 @@ const cambiarColorNombre = (nombreCompleto) => {
 };
 
 let estaPermitido = (sueldo, edad, cuotas) => {
-  if (sueldo < 30000 || edad >= 56 || cuotas >60) {
-    return false
+  if (sueldo < 30000 || edad >= 56 || cuotas > 60) {
+    return false;
   } else if (sueldo >= 30000 && edad >= 18 && edad <= 55) {
-    return true
+    return true;
   }
 };
- 
+
 const calcularCuota = (monto, cuotas) => {
   return parseInt(monto / cuotas);
 };
 
 let calcularInteres = (monto, cuotas) => {
-   if (monto <= 30000 && cuotas >= 12 && cuotas <= 18) {
+  if (monto <= 30000 && cuotas >= 12 && cuotas <= 18) {
     return 0.3;
   } else if (monto <= 75000 && cuotas >= 19 && cuotas <= 36) {
     return 0.5;
@@ -59,7 +58,7 @@ let calcularInteres = (monto, cuotas) => {
 };
 
 let calculoMontoFinal = (monto, interes) => {
-  return (monto * interes) + monto;
+  return monto * interes + monto;
 };
 
 const simularPrestamo = () => {
@@ -68,22 +67,14 @@ const simularPrestamo = () => {
   const sueldo = document.getElementById("input-sueldo").value;
   const edad = document.getElementById("input-edad").value;
   const mail = document.getElementById("input-email").value;
-  const usuario = new Usuario(
-  nombreCompleto,
-  edad,
-  sueldo,
-  mail,
-  )
+  const usuario = new Usuario(nombreCompleto, edad, sueldo, mail);
   console.debug(usuario);
 
   const monto = parseInt(document.getElementById("input-monto").value);
   const cuotas = document.getElementById("input-cuotas").value;
 
-  const prestamo = new Prestamo( 
-    monto,
-    cuotas,
-  )
- 
+  const prestamo = new Prestamo(monto, cuotas);
+
   const clienteEstaPermitido = estaPermitido(sueldo, edad, cuotas);
   if (clienteEstaPermitido) {
     const valorCuota = calcularCuota(monto, cuotas);
@@ -91,43 +82,44 @@ const simularPrestamo = () => {
     const totalidadCobrar = parseFloat(calculoMontoFinal(monto, tasaInteres));
 
     alert(`Tu prestamo de $${monto} fue aprobado en un plazo de ${cuotas} cuotas con una tasa de interes de ${tasaInteres}%. 
-    El valor de tu cuota será de $${valorCuota}. Al finalizar el período se abonará un total de $${totalidadCobrar}.`)
-
-    
+    El valor de tu cuota será de $${valorCuota}. Al finalizar el período se abonará un total de $${totalidadCobrar}.`);
 
     prestamos.push({
       nombreCompleto: usuario.nombreApellido,
       monto: prestamo.monto,
-      cuotas: prestamo.cuotas, 
-    })
-  
+      cuotas: prestamo.cuotas,
+    });
+
     const lista = document.getElementById("informe-prestamos");
     //el <ol id="informe-prestamos"> definido por html es lista en js
-    
-    const prestamosFiltrados = prestamosPorMonto();
-    // voy a ver los prestamos que cumplen: item.monto >60000
-    for (const prestamoFiltrado of prestamosFiltrados) {
+
+    const prestamosEntregados = obtenerPrestamosEntregados();
+
+    if (prestamosEntregados != null) {
+      prestamos = prestamosEntregados.concat(prestamos);
+      const prestamosFiltrados = prestamosPorMonto(prestamos);
+      // voy a ver los prestamos que cumplen: item.monto >60000
+      for (const prestamoFiltrado of prestamosFiltrados) {
         const li = document.createElement("li");
         li.innerHTML = `Nombre: ${prestamoFiltrado.nombreCompleto} 
-                          Monto: ${prestamoFiltrado.monto} 
-                          Cuotas:${prestamoFiltrado.cuotas}`
-    lista.append(li);
-    
-    };
-    localStorage.setItem("clienteSolicitantes",JSON.stringify(prestamos));
-  }
-  else {
-    alert("No cumple los requisitos para obtener un prestamo JOMARO")
-  }
+                        Monto: ${prestamoFiltrado.monto} 
+                        Cuotas:${prestamoFiltrado.cuotas}`;
+        lista.append(li);
+      }
+    }
+    localStorage.setItem("prestamosEntregados",JSON.stringify(prestamos)); //seteo en el local todos los prestamos
 
+  } else {
+    alert("No cumple los requisitos para obtener un prestamo JOMARO");
+  }
 };
 
-const prestamosPorMonto = () => {
-  const prestamosEntregados =localStorage.getItem("clienteSolicitantes");
 
-if(prestamosEntregados != null) {
-  return JSON.parse(prestamosEntregados).filter((item) => item.monto > 60000);
- }
+const obtenerPrestamosEntregados = () => {
+  return JSON.parse(localStorage.getItem("prestamosEntregados"));
+};
+const prestamosPorMonto = (prestamosEntregados) => {
+  return prestamosEntregados.filter((item) => item.monto > 60000);
 };
 
 let boton = document.getElementById("boton");
